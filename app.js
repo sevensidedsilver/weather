8a437b33fea7f9da9cd1f977aedcae2e08e6fbac
Sean <sevensidedsilver@gmail.com>
2017-04-29 10:36:32 -0600

    angular controller configuration

---
+++ app.js
@@ -0,0 +1,11 @@
+var weatherApp = angular.module("weatherApp", []);
+
+//weather API: dac7217df20b19b9419d52579963e0ee
+
+
+ninjaApp.controller('ninjaController', ["$scope", function($scope){
+  // $scope.message = "Test Message";
+
+
+//closes controller
+}]);
